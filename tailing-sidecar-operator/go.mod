547ed26ff0f6731b7f26c23c91866437c3834262
yashoza19 <yoyashoza@gmail.com>
2021-06-28 14:22:54 -0400

    Modifying tailing-sidecar-operator image

--- tailing-sidecar-operator/go.mod
+++ tailing-sidecar-operator/go.mod
@@ -5,10 +5,11 @@ go 1.13
 require (
 	github.com/go-logr/logr v0.4.0
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/micro/micro/v3 v3.3.0 // indirect
 	github.com/nsf/jsondiff v0.0.0-20200515183724-f29ed568f4ce
-	github.com/onsi/ginkgo v1.15.1
-	github.com/onsi/gomega v1.11.0
-	gomodules.xyz/jsonpatch/v2 v2.1.0
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.13.0
+	gomodules.xyz/jsonpatch/v2 v2.2.0
 	k8s.io/api v0.20.2
 	k8s.io/apimachinery v0.21.1
 	k8s.io/client-go v0.20.2
--- tailing-sidecar-operator/tests/test-update.sh
+++ tailing-sidecar-operator/tests/test-update.sh
@@ -23,10 +23,10 @@ readonly DEPLOYMENT_POD_NAME="$(kubectl get pod -l app=deployment-with-annotatio
 wait_for_pod ${NAMESPACE} ${DEPLOYMENT_POD_NAME} ${TIME}
 [[ $(kubectl logs ${DEPLOYMENT_POD_NAME} tailing-sidecar-0 -n ${NAMESPACE} --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
-# Test Pod with configuration in CRD
-readonly POD_WITH_CRD="pod-with-annotations-crd"
-wait_for_pod ${NAMESPACE} ${POD_WITH_CRD} ${TIME}
-[[ $(kubectl logs ${POD_WITH_CRD} sidecar-2 -n ${NAMESPACE} --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+# Test Pod with configuration in CR
+readonly POD_WITH_CR="pod-with-tailing-sidecar-config"
+wait_for_pod ${NAMESPACE} ${POD_WITH_CR} ${TIME}
+[[ $(kubectl logs ${POD_WITH_CR} sidecar-2 -n ${NAMESPACE} --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 echo "ok"
 exit 0
